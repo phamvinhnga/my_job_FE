fb5aa9d76be54625ec39d4f594c6b278ce131a63
nga.vinhpham <101375733+ngaphaminfodation@users.noreply.github.com>
2023-02-21 14:09:47 +0700

    x

--- src/app/module/cms/components/post/edit/edit.component.ts
+++ src/app/module/cms/components/post/edit/edit.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+import { SummernoteOptions } from 'ngx-summernote/lib/summernote-options';
+import { BaseComponent } from 'src/app/module/shared/model/BaseComponent.model';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss']
 })
-export class PostEditComponent implements OnInit {
+export class PostEditComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  summernoteOptionsSummery:SummernoteOptions = {};
+
+  constructor() {
+    super();
+  }
 
   ngOnInit(): void {
+    this.summernoteOptionsSummery = _.cloneDeep(this.summernoteOptions);
+    this.summernoteOptionsSummery.height = 100;
+    this.summernoteOptions.height = 500;
   }
 
 }
--- src/app/module/shared/model/baseComponent.model.ts
+++ src/app/module/shared/model/baseComponent.model.ts
@@ -1,59 +1,69 @@
 import { SummernoteOptions } from "ngx-summernote/lib/summernote-options";
-import * as _ from 'lodash';    
+import * as _ from 'lodash';
 import { FileModel, IFileModel } from "./file.model";
+import { FormGroup } from "@angular/forms";
 
 export class BaseComponent {
 
-    summernoteOptions:SummernoteOptions = {
-        placeholder: '',
-        tabsize: 2,
-        toolbar: [
-            ['misc', ['codeview', 'undo', 'redo']],
-            ['style', ['bold', 'italic', 'underline', 'clear']],
-            ['font', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
-            ['fontsize', ['fontname', 'fontsize', 'color']],
-            ['para', ['style', 'ul', 'ol', 'paragraph', 'height']],
-            ['insert', ['table', 'picture', 'link', 'video', 'hr']]
-        ],
-        popover: {
-          image: [
-            ['image', ['resizeFull', 'resizeHalf', 'resizeQuarter', 'resizeNone']],
-            ['float', ['floatLeft', 'floatRight', 'floatNone']],
-            ['remove', ['removeMedia']]
-          ],
-          link: [
-            ['link', ['linkDialogShow', 'unlink']]
-          ],
-          table: [
-            ['add', ['addRowDown', 'addRowUp', 'addColLeft', 'addColRight']],
-            ['delete', ['deleteRow', 'deleteCol', 'deleteTable']],
-          ],
-          air: [
-            ['color', ['color']],
-            ['font', ['bold', 'underline', 'clear']],
-            ['para', ['ul', 'paragraph']],
-            ['table', ['table']],
-            ['insert', ['link', 'picture']]
-          ]
-        }
+  form: FormGroup | undefined;
+  summernoteOptions: SummernoteOptions = {
+    placeholder: '',
+    tabsize: 2,
+    toolbar: [
+      ['misc', ['codeview', 'undo', 'redo']],
+      ['style', ['bold', 'italic', 'underline', 'clear']],
+      ['font', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+      ['fontsize', ['fontname', 'fontsize', 'color']],
+      ['para', ['style', 'ul', 'ol', 'paragraph', 'height']],
+      ['insert', ['table', 'picture', 'link', 'video', 'hr']]
+    ],
+    popover: {
+      image: [
+        ['image', ['resizeFull', 'resizeHalf', 'resizeQuarter', 'resizeNone']],
+        ['float', ['floatLeft', 'floatRight', 'floatNone']],
+        ['remove', ['removeMedia']]
+      ],
+      link: [
+        ['link', ['linkDialogShow', 'unlink']]
+      ],
+      table: [
+        ['add', ['addRowDown', 'addRowUp', 'addColLeft', 'addColRight']],
+        ['delete', ['deleteRow', 'deleteCol', 'deleteTable']],
+      ],
+      air: [
+        ['color', ['color']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['para', ['ul', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture']]
+      ]
     }
+  };
 
-    async onHandleUpload(event:any) : Promise<FileModel[]>{
-        let result:FileModel[] = [];
-         _.each(event?.target?.files, async (file:File) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            await new Promise((resolve, reject) => {
-                reader.onload = () => {
-                    result.push({
-                        name: file.name,
-                        type: file.type,
-                        url: reader.result
-                    } as IFileModel)
-                };
-            });
-       
-        });
-        return result;
+  listFileImage:IFileModel[] = [];
+
+  async onHandleUploadImage(event: any): Promise<FileModel[]> {
+    if(!this.listFileImage){
+      this.listFileImage = [];
     }
+    _.each(event?.target?.files, async (file: File) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      await new Promise((resolve, reject) => {
+        reader.onload = () => {
+          this.listFileImage.push({
+            name: file.name,
+            type: file.type,
+            url: reader.result
+          } as IFileModel)
+        };
+      });
+
+    });
+    return this.listFileImage;
+  }
+
+  onRemoveFileImage(){
+    this.listFileImage = [];
+  }
 }
